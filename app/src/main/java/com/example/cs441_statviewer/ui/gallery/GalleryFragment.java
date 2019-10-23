63713c766a4b3c1af7af7c5e836479ab41c26766
Kevin Wallace <kjwall97@yahoo.com>
2019-10-22 23:57:24 -0400

    Created Standings table in gallery fragment and added code to populate the team names column

--- app/src/main/java/com/example/cs441_statviewer/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/cs441_statviewer/ui/gallery/GalleryFragment.java
@@ -1,9 +1,12 @@
 package com.example.cs441_statviewer.ui.gallery;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -20,6 +23,7 @@ public class GalleryFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        Context context = getContext();
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
@@ -30,6 +34,14 @@ public class GalleryFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        final TableLayout NARLCSStandingsTable = root.findViewById(R.id.na_rlcs_standings_table);
+        String[] NARLCSTeamNames = context.getResources().getStringArray(R.array.NA_RLCS_Teams);
+        for(int i=1; i < NARLCSStandingsTable.getChildCount(); i++) {
+            TableRow curRow = (TableRow) NARLCSStandingsTable.getChildAt(i);
+            TextView teamName = (TextView) curRow.getChildAt(0);
+
+            teamName.setText(NARLCSTeamNames[i-1]);
+        }
         return root;
     }
 }
