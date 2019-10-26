b170a5ff419f8785680998c54a30badff2c112cc
Kevin Wallace <kjwall97@yahoo.com>
2019-10-26 19:43:46 -0400

    Fixed table crash, added borders to cells. Ready to extend data to other tables on other menus maybe!

--- app/src/main/java/com/example/cs441_statviewer/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/cs441_statviewer/ui/slideshow/SlideshowFragment.java
@@ -1,10 +1,13 @@
 package com.example.cs441_statviewer.ui.slideshow;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -16,23 +19,15 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cs441_statviewer.R;
+import com.google.android.material.tabs.TabLayout;
+
+import org.w3c.dom.Text;
 
 public class SlideshowFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         // Create array of teams
         // Create array of ELO ratings
@@ -42,32 +37,36 @@ public class SlideshowFragment extends Fragment {
         String[] elos = context.getResources().getStringArray(R.array.global_top_50_elos);
         TableLayout eloTable = root.findViewById(R.id.global_elo_table);
 
-        ViewGroup.LayoutParams textViewParams = new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-        );
 
-        for(int i=0; i < 1; i++) {
+        for(int i=0; i < teams.length; i++) {
             TableRow newRow = new TableRow(context);
-            TextView ranking = new TextView(context);
-            TextView teamName = new TextView(context);
-            TextView elo = new TextView(context);
-            TextView region = new TextView(context);
-            ranking.setText(Integer.toString(i+1));
-            ranking.setLayoutParams(textViewParams);
-            teamName.setText(teams[i]);
-            teamName.setLayoutParams(textViewParams);
-            region.setText(regions[i]);
-            region.setLayoutParams(textViewParams);
-            elo.setText(elos[i]);
-            elo.setLayoutParams(textViewParams);
-            newRow.addView(ranking);
-            newRow.addView(teamName);
-            newRow.addView(region);
-            newRow.addView(elo);
-            eloTable.addView(eloTable);
+            newRow.setBackgroundColor(Color.BLACK);
+            newRow.addView(makeFormattedTextView(Integer.toString(i+1)));
+            newRow.addView(makeFormattedTextView(teams[i]));
+            newRow.addView(makeFormattedTextView(regions[i]));
+            newRow.addView(makeFormattedTextView(elos[i]));
+            eloTable.addView(newRow);
         }
 
         return root;
+    }
+
+    private LinearLayout makeFormattedTextView(String text) {
+        // Have to do layout params of Parent class!!
+        TableRow.LayoutParams rowParams = new TableRow.LayoutParams();
+        rowParams.width = TableRow.LayoutParams.MATCH_PARENT;
+        rowParams.height = TableRow.LayoutParams.MATCH_PARENT;
+        rowParams.setMargins(2,2,2,2);
+
+        LinearLayout layout = new LinearLayout(getContext());
+        layout.setBackgroundColor(Color.WHITE);
+        layout.setLayoutParams(rowParams);
+
+        TextView textView = new TextView(getContext());
+        layout.addView(textView);
+        textView.setGravity(Gravity.CENTER_HORIZONTAL);
+        textView.setText(text);
+
+        return layout;
     }
 }
